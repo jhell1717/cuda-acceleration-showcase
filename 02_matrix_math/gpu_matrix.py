115ccb22d002e49575fb2159d15fda609999090e
jhell1717 <jhellewell1722@gmail.com>
2025-12-14 11:55:20 +0000

    updated GPU matrix multiplication

--- 02_matrix_math/gpu_matrix.py
+++ 02_matrix_math/gpu_matrix.py
@@ -1,6 +1,6 @@
 from numba import cuda
 import numpy as np
-
+import time
 
 
 @cuda.jit
@@ -20,9 +20,17 @@ def gpu_matrix_multiply(A, B):
 
     d_A = cuda.to_device(A)
     d_B = cuda.to_device(B)
+    #Specify dimensions to match original image.
     d_C = cuda.device_array((A.shape[0], B.shape[1]), dtype=np.float32)
 
     threads_per_block = (16, 16)
     blocks_per_grid = (
         (A.shape[0] + threads_per_block[0] - 1) // threads_per_block[0],
         (B.shape[1] + threads_per_block[1] - 1) // threads_per_block[1])
+
+    start = time.time()
+    matrix_multiply_kernel[blocks_per_grid, threads_per_block](d_A,d_B,d_C)
+    cuda.synchronize()
+    end = time.time()
+
+    print(f'GPU Matrix Multiplicatoin Time: {end-start} seconds')
