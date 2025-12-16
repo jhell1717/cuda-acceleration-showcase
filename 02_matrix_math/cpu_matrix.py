5e7c1c36dd1c0bcfa99190ae65a496297311b1bc
jhell1717 <jhellewell1722@gmail.com>
2025-12-16 16:55:25 +0000

    update cpu time

--- 02_matrix_math/cpu_matrix.py
+++ 02_matrix_math/cpu_matrix.py
@@ -6,5 +6,5 @@ def cpu_matrix_multiply(A,B):
     result = np.dot(A,B)
     end = time.time()
 
-    print(f'GPU Matrix Multiplication Time: {end-start} seconds')
+    print(f'CPU Matrix Multiplication Time: {end-start} seconds')
     return result
